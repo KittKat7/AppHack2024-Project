1e092e1547f394fad5cdc3db9193547299a3f905
BOB <carenathan027@gmail.com>
2024-03-23 19:44:02 -0400

    added --cli flag to disable tts

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import random
+import sys
 from texttospeach import *
 from query import *
 
+speechmode=True
+if "--cli" in  sys.argv:
+        speechmode=False
+
+def output(text):
+    print(text)
+    if speechmode:
+        speak(text)
 # Define a function to greet the user
 def greet():
-    greetings = ["Hello!", "Hi there!", "Hey!"]
+    greetings = ["Hi there!", "Hey!", "Good morning!","Good afternoon!",
+"Greetings!","Howdy!","What's up?","Yo!","Hiya!","Salutations!","How's it going?",
+"Well met!","Hi, how are you?","Hello there!","Hey, what's happening?","How are you doing?",
+"What's new?","Hi, how's your day?"]
     greet_msg = random.choice(greetings)
-    print(greet_msg)
-    speak(greet_msg)
-
+    output(greet_msg)
 
 # Define a function to take user input and respond accordingly
 def respond_to_user_input(user_input):
     response = query(user_input)
-    print(response)
-    speak(response)
+    output(response)
     return
 
 # Main function to run the virtual assistant
@@ -22,9 +31,8 @@ def main():
     greet()
     while True:
         user_input = input("You: ").lower()
-        end_chat = respond_to_user_input(user_input)
-        if end_chat:
-            break
+        respond_to_user_input(user_input)
+
 
 if __name__ == "__main__":
     main()
