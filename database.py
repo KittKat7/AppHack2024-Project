fe2f3faeb2605a946cd2fa3b45930943bd76a965
kittkat7 <rainbowtabby7@gmail.com>
2024-03-23 18:34:41 -0400

    set up init and insert for database

--- database.py
+++ database.py
@@ -1,6 +1,11 @@
 import sqlite3
 
-def init():
+cursor: sqlite3.Cursor
+connection: sqlite3.Connection
+
+def initdb():
+	global connection
+	global cursor
 	connection = sqlite3.connect('NebulaOffline.db')
 	cursor = connection.cursor()
 	cursor.execute('''
@@ -36,10 +41,30 @@ def init():
 				question INTEGER,
 				body TEXT,
 				score INTEGER,
-				isAccepter INTEGER,
+				isAccepted INTEGER,
 				FOREIGN KEY question REFERENCES question(id)
 				)
 				''')
+	connection.commit()
+#initdb
 
+def insertQuestion(id: int, title: str, link: str, score: int, answered: bool, tags: list[str]):
+	global connection
+	global cursor
+	answered: bool = 1 if answered else 0
+	cursor.execute("INSERT OR IGNORE INTO question (id, title, link, score, answered) VALUES (?, ?, ?, ?, ?)", (id, title, link, score, answered))
+	for tag in tags:
+		cursor.execute("INSERT OR IGNORE tag (tag) VALUES (?)", (tag))
+		cursor.execute("INSERT OR IGNORE hasTag (question, tag) VALUES (?, ?)", (id, tag))
+	connection.commit()
+#insertQuestion
+
+def insertAnswer(id: int, question: int, body: str, score: int, accepted: bool):
+	global connection
+	global cursor
+	accepted: bool = 1 if accepted else 0
+	cursor.execute("INSERT OR IGNORE INTO answer (id, question, body, score, isAccepted) VALUES (?, ?, ?, ?, ?)", (id, question, body, score, accepted))
+	connection.commit()
+#insertAnswer
 
 
